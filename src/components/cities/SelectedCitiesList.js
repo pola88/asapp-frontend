4c12ac2d50154e75ee540328eb0f4d4dbd6ea2eb
pola88 <pola88@gmail.com>
2021-10-08 17:12:43 -0300

    Show selected city as Chip

--- src/components/cities/SelectedCitiesList.js
+++ src/components/cities/SelectedCitiesList.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectSelectedCities } from '../../features/cities/citySlice';
 import { map, values, isEmpty } from 'ramda';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 export function SelectedCitiesList() {
   const selectedCities = useSelector(selectSelectedCities);
-  const selectedRow = map(city => <div key={`selected_${city.geonameid}`}>{city.name} ({city.subcountry})</div>, values(selectedCities));
+  const selectedRow = map(city => <Chip key={`selected_${city.geonameid}`} label={`${city.name} (${city.subcountry})`} />, values(selectedCities));
+  
   return (<div>
     <div>Selected cities:</div>
     { isEmpty(selectedRow)
       ? <div>You haven't selected any city yet</div>
-      : selectedRow
+      : (<Stack direction="row" spacing={1}>
+          {selectedRow}
+        </Stack>)
     }
   </div>);
 };
