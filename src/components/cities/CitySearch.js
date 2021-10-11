a7cff973221cbc378ed8c27b9f52f97f75b66cd2
pola88 <pola88@gmail.com>
2021-10-11 18:04:04 -0300

    Add debounce on the input, add helper text

--- src/components/cities/CitySearch.js
+++ src/components/cities/CitySearch.js
@@ -5,11 +5,14 @@ import Input from '@mui/material/Input';
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import FormHelperText from '@mui/material/FormHelperText';
+import { useDebouncedCallback } from 'use-debounce';
 
 export function CitySearch() {
   const dispatch = useDispatch();
 
-  const searchCity = (event) => {
+  //Use debounce func to handle when the user write fast into the input and don't send multiple request
+  const searchCity = useDebouncedCallback((event) => {
     const value = event.target.value;
     if (value.length >= 3) {
       dispatch(fetchBy(value));
@@ -18,19 +21,23 @@ export function CitySearch() {
     }
 
     dispatch(searchBy(value));
-  };
+  }, 500);
 
   return (
     <FormControl variant="standard">
       <Input
         id="input-with-icon-adornment"
         onChange={searchCity}
+        placeholder="Search city..."
         startAdornment={
           <InputAdornment position="start">
             <FilterAltIcon />
           </InputAdornment>
         }
       />
+      <FormHelperText id="component-helper-text">
+        Min 3 characters
+      </FormHelperText>
     </FormControl>
   )
 }
